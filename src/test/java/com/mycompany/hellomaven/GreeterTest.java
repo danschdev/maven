e924cbc8d976d0e0da424709b65d208f24398463
dschnelle <daniel_schnelle@web.de>
2017-06-17 17:16:02 +0200

    Create output test for Greeter class

---
+++ src/test/java/com/mycompany/hellomaven/GreeterTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.hellomaven;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author daniel
+ */
+public class GreeterTest {
+    
+    public GreeterTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of sayHello method, of class Greeter.
+     */
+    @Test
+    public void testSayHello() {
+        System.out.println("sayHello");
+        Greeter instance = new Greeter();
+        String expResult = "Hello Barmbek!";
+        String result = instance.sayHello();
+        assertEquals(expResult, result);
+    }
+    
+}
