f190fd59a7a073127c6f6e4119a22c35dc142fb0
dschnelle <daniel_schnelle@web.de>
2017-07-11 20:37:26 +0200

    Add unit tests for Cube methods

--- src/test/java/danschdev/rubik/CubeTest.java
+++ src/test/java/danschdev/rubik/CubeTest.java
@@ -37,5 +37,93 @@ public class CubeTest {
         Color result = instance.getFront();
         assertEquals(expResult, result);
     }
+
+    /**
+     * Test of turnFromTo method, of class Cube.
+     */
+    @Test
+    public void testTurnFromTo() throws Exception {
+        System.out.println("turnFromTo");
+        Side start = Side.front;
+        Side target = Side.up;
+        Color frontColor = start.getColor();
+        Cube instance = new Cube();
+        instance.turnFromTo(start, target);
+               
+        Color targetColor = target.getColor();
+        assertEquals(frontColor, targetColor);
+    }
+
+    /**
+     * Test of getFront method, of class Cube.
+     */
+    @Test
+    public void testGetFront() {
+        System.out.println("getFront");
+        Cube instance = new Cube();
+        Color expResult = Color.GREEN;
+        Color result = instance.getFront();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getBack method, of class Cube.
+     */
+    @Test
+    public void testGetBack() {
+        System.out.println("getBack");
+        Cube instance = new Cube();
+        Color expResult = Color.YELLOW;
+        Color result = instance.getBack();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getLeft method, of class Cube.
+     */
+    @Test
+    public void testGetLeft() {
+        System.out.println("getLeft");
+        Cube instance = new Cube();
+        Color expResult = Color.PURPLE;
+        Color result = instance.getLeft();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getRight method, of class Cube.
+     */
+    @Test
+    public void testGetRight() {
+        System.out.println("getRight");
+        Cube instance = new Cube();
+        Color expResult = Color.BLUE;
+        Color result = instance.getRight();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getUp method, of class Cube.
+     */
+    @Test
+    public void testGetUp() {
+        System.out.println("getUp");
+        Cube instance = new Cube();
+        Color expResult = Color.RED;
+        Color result = instance.getUp();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDown method, of class Cube.
+     */
+    @Test
+    public void testGetDown() {
+        System.out.println("getDown");
+        Cube instance = new Cube();
+        Color expResult = Color.PINK;
+        Color result = instance.getDown();
+        assertEquals(expResult, result);
+    }
     
 }
